0a6f97019fa8a43883ddd655b63a7d17007e237c
Jose <jfcatarino1@gmail.com>
2017-04-19 16:02:01 -0600

    v2.1
    added setup function components. Tested to work.

--- stepper-motor-test.ino
+++ stepper-motor-test.ino
@@ -6,9 +6,10 @@ Previous versions:
   v0                     // unedited code from Adafruit, 4/18/17
   v0.1                   // moved step and speed to loop, 4/18/17
   v1.0                   // added BNO055 sensor input, 4/18/17
+  v2.0                   // replaced the Stepper.h library with Accelstepper.h, 4/19/17
 
 Current Version:
-  v2.0                   // replaced the Stepper.h library with Accelstepper.h, 4/19/17
+  v2.1                   // added setup function components, 4/10/17
 */
 
 //set up for the BNO055 sensor
@@ -21,6 +22,7 @@ Adafruit_BNO055 bno = Adafruit_BNO055();
 
 //set up the stepper motor library
 #include <AccelStepper.h>
+#define HALF4WIRE 8
 
 //set up pins for the L293D
 #define motorPin1  2
@@ -28,12 +30,12 @@ Adafruit_BNO055 bno = Adafruit_BNO055();
 #define motorPin3  6
 #define motorPin4  7
 
+AccelStepper stepper1 (HALF4WIRE, motorPin1, motorPin2, motorPin3, motorPin4, true);
+
 //set up constants for sensor input
 int rotSpeed = 0; //angular velocity value from BNO055 sensor
-int speedC = 10; //a scaling factor
+int speedC = 500; //a scaling factor
 int rotDist = 0; //"steps" always either 500, -500, or 0
-
-AccelStepper stepper1 (HALF4WIRE, motorPin1, motorPin2, motorPin3, motorPin4, true);
 
 void setup()
 {
@@ -50,6 +52,12 @@ void setup()
   }
 
   delay(1000);
+
+  stepper1.setMaxSpeed(12000.0);
+  stepper1.setAcceleration(1000.0);
+  stepper1.setSpeed(500);
+  stepper1.moveTo(12000); //250 full rotations @ 48 steps each = 12,000 steps
+
 }
 
 void loop()
@@ -60,9 +68,9 @@ void loop()
   // angular velocity * a scaling factor
   rotSpeed = gyro.z() * speedC;
 
-  // write speed value
+  // set the speed for the motor
   stepper1.setSpeed(rotSpeed);
 
-  //run stepper motor
+  // start the motor
   stepper1.run();
 }
