f87a7888990c1418908d82c858102fa8342083ec
Jose <jfcatarino1@gmail.com>
2017-04-19 00:34:27 -0600

    v1
    added BNO055 sensor input

--- stepper-motor-test.ino
+++ stepper-motor-test.ino
@@ -1,6 +1,13 @@
 /*
 Adapted by Jose Catarino from:
 Adafruit Arduino - Lesson 16. Stepper
+
+Previous versions:
+  v0                     // unedited code from Adafruit
+  v0.1                   // moved step and speed to loop
+
+Current Version:
+  v1                     // added BNO055 sensor input
 */
 
 //set up for the BNO055 sensor
@@ -23,8 +30,7 @@ int in4Pin = 9;
 //set up constants for sensor input
 int rotSpeed = 0; //angular velocity value from BNO055 sensor
 int speedC = 10; //constant of proportionality for angular velocity
-int rotDist = 0; //degrees turned by sensor
-int distC = 100;//constant of proportionality for degrees turned
+int rotDist = 0; //"steps" always either 500, -500, or 0
 
 Stepper motor(512, in1Pin, in2Pin, in3Pin, in4Pin);
 
@@ -55,20 +61,23 @@ void loop()
   //set up the get gyroscope function
   imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
 
-  rotSpeed = abs(gyro.z() * speedC); // andgular velocity from sensor, gyroscope
+  rotSpeed = abs(gyro.z() * speedC); // absolute value of the angular velocity x a sclaing factor
 
+  //if sensor is spinning cw, turn stepper in cw direction
   if ((gyro.z() * speedC) > 5) {
     rotDist = 500;
   }
+  //if sensor is spinning ccw, turn stepper in ccw direction
   else if ((gyro.z() * speedC) < -5) {
     rotDist = -500;
   }
+  //is sensor is relatively still, don't turn
   else {
     rotDist = 0;
   }
 
-  motor.setSpeed(rotSpeed);
-  motor.step(rotDist);
+  motor.setSpeed(rotSpeed); //set speed proportional to angular velocity of sensor
+  motor.step(rotDist); //control cw vs ccw direction
 
   Serial.print(rotSpeed);
   Serial.print("   ");
