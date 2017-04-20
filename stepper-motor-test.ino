1121bfbb07dd657e1cf2d5c4f0a8ab4bd6e2b1ef
Jose <jfcatarino1@gmail.com>
2017-04-20 14:56:38 -0600

    v3.2.1
    full rotation bug fix. Untested.

--- stepper-motor-test.ino
+++ stepper-motor-test.ino
@@ -1,49 +1,57 @@
 /*
-Adapted by Jose Catarino from:
-  Adafruit Arduino - Lesson 16. Stepper
-  Adafruit BNO055 Overview Learning Guie
+Camera Stabilization System by Jose Catarino
+    BNO055 code from Adafruit BNO055 Overview Learning Guide
 
-connections
+You need:
+       L293D IC Chip: H-Bridge Circuit
+       stepper motor: https://www.adafruit.com/product/918
+       BNO055 Position Sensor: https://www.adafruit.com/product/2472
+
+Connections:
   L293D and Stepper Motor:
-        +5V              1|      |9     +5V
-        Pin 2            2|      |10    Pin 7
-        Stepper blue     3|      |11    Stepper orange
-        GND              4|      |12    GND
-        GND              5|      |13    GND
-        Stepper yellow   6|      |14    Stepper pink
-        Pin 4            7|      |15    Pin 6
-        +5V              8|      |16    +5V
+        +5V               1|      |9     +5V
+        Pin 2             2|      |10    Pin 7
+        Stepper blue      3|      |11    Stepper orange
+        GND               4|      |12    GND
+        GND               5|      |13    GND
+        Stepper yellow    6|      |14    Stepper pink
+        Pin 4             7|      |15    Pin 6
+        +5V               8|      |16    +5V
 
-  SDA is orange
-  SCL is yellow
+  stepper mottor red wire left unconnected
+
+  Connect SDA with orange wire
+          SCL with yellow wire
 
 Previous versions:
-  v0                     // unedited code from Adafruit, 4/18/17
+  v0                     // unedited code from Adafruit and AccelStepper.h library, 4/18/17
   v0.1                   // moved step and speed to loop, 4/18/17
   v1.0                   // added BNO055 sensor input, 4/18/17
   v2.0                   // replaced the Stepper.h library with Accelstepper.h, 4/19/17
   v2.1                   // added setup function components, 4/19/17
   v3.0                   // included pitch control, 4/19/17
   v3.1                   // scaling factor adjustments, 4/19/17
+  v3.2                   // removed pitch correction, 4/19/17
+
 
 
 Current Version:
-  v3.1.1                  // no pitch correction, 4/19/17
+  v3.2.1                 // full rotation bug fix, 4/20/17
 */
 
-//set up for the BNO055 sensor
+// libraries needed for BNO055 position sensor
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
+#include <Adafruit_BNO055.h> // https://learn.adafruit.com/adafruit-bno055-absolute-orientation-sensor/overview
 #include <utility/imumaths.h>
 #include <Servo.h>
 Adafruit_BNO055 bno = Adafruit_BNO055();
 
-//set up the stepper motor library
-#include <AccelStepper.h>
+// libraries needed for stepper motor
+#include <AccelStepper.h>    // http://www.airspayce.com/mikem/arduino/AccelStepper/index.html
 #define HALF4WIRE 8
 
-//set up pins for the L293D for stepper motor
+// pins for the L293D for stepper motor
 #define motorPin1  2
 #define motorPin2  4
 #define motorPin3  6
@@ -51,13 +59,13 @@ Adafruit_BNO055 bno = Adafruit_BNO055();
 AccelStepper stepper1 (HALF4WIRE, motorPin1, motorPin2, motorPin3, motorPin4, true);
 
 
-//set up constants for sensor input
-int rotSpeed = 0; // angular velocity value from BNO055 sensor
-int speedC = 200; // a scaling factor for speed
+// variables for sensor-controlled motor motion
+int rotSpeed = 0;            // angular velocity value from BNO055 sensor
+int speedC = 200;            // a scaling factor for speed
 
 void setup()
 {
-  //Initialise the serial monitor
+  // Initialise the serial monitor
   Serial.begin(9600);
 
   // Initialise the sensor
@@ -70,16 +78,14 @@ void setup()
 
   delay(1000);
 
+  // Initialise stepper motor values
   stepper1.setMaxSpeed(12000.0);
-  stepper1.setAcceleration(1000.0);
   stepper1.setSpeed(500);
-  stepper1.moveTo(120000);
-
 }
 
 void loop()
 {
-  // set up the get gyroscope function, angular velocity rads/sec
+  // gyroscope function, angular velocity rads/sec
   imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
 
   // angular velocity * a scaling factor
@@ -89,5 +95,5 @@ void loop()
   stepper1.setSpeed(rotSpeed);
 
   // start the motor
-  stepper1.run();
+  stepper1.runSpeed();
 }
